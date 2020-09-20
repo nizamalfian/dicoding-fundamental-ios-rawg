c30f5960fa5b6a8f5872032358eac5f8e73b6fa9
Amirul Nizam Alfian <amirulnizamalfian@gmail.com>
2020-09-21 01:17:42 +0700

    fix: remove error code

--- Dicoding - RAWG/Base/AppDelegate.swift
+++ Dicoding - RAWG/Base/AppDelegate.swift
@@ -15,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        UINavigationBar.appearance().titleTextAttributes = [NSFileProviderSyncAnchor:UIColor.whiteColor()]
         return true
     }
 
